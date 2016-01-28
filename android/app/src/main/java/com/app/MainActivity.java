072101a27e5006ece1f3bdf6c649aa358d7cc0b1
KangChiaPin <billy5521@gmail.com>
2016-01-28 16:28:36 +0800

    fix project name

--- android/app/src/main/java/com/app/MainActivity.java
+++ android/app/src/main/java/com/app/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends Activity implements DefaultHardwareBackBtnHand
                 .setInitialLifecycleState(LifecycleState.RESUMED)
                 .build();
 
-        mReactRootView.startReactApplication(mReactInstanceManager, "App", null);
+        mReactRootView.startReactApplication(mReactInstanceManager, "plant", null);
 
         setContentView(mReactRootView);
     }
