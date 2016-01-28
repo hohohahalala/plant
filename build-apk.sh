542c310159a1256e6c92fe6291f5750533886f88
hohohahalala <hohohahalala1223@gmail.com>
2016-01-28 15:03:27 +0800

    new begin

--- build-apk.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-curl "http://localhost:8081/index.android.bundle?platform=android" -o "android/app/src/main/assets/index.android.bundle"
-cd android
-./gradlew assembleRelease
-./gradlew installRelease
-
--- ios/AppTests/AppTests.m
+++ ios/AppTests/AppTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 240
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface plantTests : XCTestCase
+@interface AppTests : XCTestCase
 
 @end
 
-@implementation plantTests
+@implementation AppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
