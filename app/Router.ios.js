a6a8a455860f319b0670f53b62ee82252fe9d787
KangChiaPin <billy5521@gmail.com>
2016-01-30 16:54:33 +0800

    try to use listview

--- app/Router.ios.js
+++ app/Router.ios.js
@@ -5,19 +5,33 @@ import React, {
   View,
   Navigator,
   Text,
+  ListView,
   Component,
 } from 'react-native';
 
 class plant extends Component {
+  getInitialState() {
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    return {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+    };
+  }
   renderScene(route, navigator) {
     switch(route.id) {
       case 'Main':
         return (
           <View>
-            <Text> Main </Text>
+            <Text> {this.state.dataSource} </Text>
+            <ListView
+              dataSource={this.state.dataSource}
+              renderRow={this._renderRow}
+              />
           </View>
         );
     }
+  }
+  _renderRow(rowData){
+    return <Text>{rowData}</Text>;
   }
   render() {
     return (
